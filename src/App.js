8610420050674bf140d5ec758956b1818e4acb4e
mothukurilakshmithirapathamma <mothukurilakshmi97@gmail.com>
2024-04-20 21:51:18 +0530

    add files

---
+++ src/App.js
@@ -0,0 +1,57 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+
+      <h2 id="chatrapathi"><ins>chatrapathi</ins></h2>
+    <a href="chatrapathi.html">
+        <img src="./chatrapathi/cp.avif"width="300px"/>
+    </a>
+    <p>Release Date : May 12, 2023
+        Shivaji and his family are uprooted from their community in Sri Lanka and wind up as bonded labourers in Visakhapatnam. The situation worsens when his jealous stepbrother enters his life.
+
+     <h2 id="tiger nageswara rao"><ins>tiger nageswara rao</ins></h2>
+    <div id="tiger nageswara rao"></div>
+    <a href="tiger nageswara rao"><marquee>
+    <img src="./tnr/t.jpg"width="300px"/></marquee></a>
+    <p id="paragraph">
+        A legendary real-life robber of Stuartpuram, Andhra Pradesh called Tiger Nageswara Rao in the 1970s, who was noted for his ingenious ways of evading the police and prison.
+
+
+Director
+Vamsee
+Writers
+VamseeSrikanth Vissa
+Stars
+
+Sai Srinivas BellamkondaNushrratt BharucchaKaran Singh Chhabra
+See production info at IMDbPro</p>
+       <h1 id="vimanam">vimanam</h1>
+    <a href="vimanam"></a>
+    <img src='./vimanam/vm.jpg' width="300px"/>
+    <p>
+       Vimanam (transl. Airplane) is a 2023 Indian Telugu-language drama film written and directed Siva Prasad Yanala.[1][2] It features Samuthirakani and Master Dhruvan in lead roles[3] It was produced by Kiran Korrapati and Zee Studios.[4]
+
+The film was released on 9 June 2023 and was partially filmed in Tamil.
+
+Plot
+Veerayya runs the Sulabh Complex in a Hyderabad slum, near begumpet. His life revolves around his son Raju. Veerayya has no wife and no relatives, except for three neighbours: Daniel, a kind-hearted auto driver; Koti, a witty cobbler; and Sumathi, a compassionate sex worker. One day, Veerayya learns that Raju has leukemia and only a few days to live. Raju loves planes wants to fly in aeroplane. Determined to fulfil his son’s wish, Veerayya works day and night to save enough money to buy two plane tickets to Tirupati.He works as a carpenter and asks Rs 10000 from the boss .But he denies. On that day a sum of Rs 50000 is lost from the Boss account. So the boss complaints to police and veerayya goes to jail. For that the boss pays Rs 10000 as bribe. In lock up, the police beats him. The next day the boss finds out that the money was taken by his son. So he withdraws his complaint and gives Rs 5000 as bribe for that. The next day veerayas son and Daniels son are caught by Airport security that they were trying to cross the fence and they were brought to police station. The police who beat Veerayya goes to beat Raju but Veerayya stops him and threatens the police that he will go to higher officials and file a complain. He acts as a comedian in am exhibition and makes the children laugh. He gets paid a lump sum of money .Veeraiyya and his friends go to buy ticket at night time . Some rich guys drove on wrong way and hit Daniels auto and fough with him and </p>
+
+
+Ravi TejaAnupam KherGayatri Bhardwaj
+See production info at IMDbPro
+When director Vamsee’s ambitiously mounted Telugu film Tiger Nageswara Rao (TNR) begins with a note ‘inspired from true rumours’ and a voiceover informs us that it is a tale of blood and tears, it indicates the tone of the film. Ravi Teja plays the title character inspired by the real-life dacoit of Stuartpuram in Andhra Pradesh, Nageswara Rao who earned the moniker ‘tiger’ for his daredevil acts. In the absence of substantial documentation, the director and his team relied on accounts shared by the late dacoit’s wife and a few others in the village and the police. Sharing the writing credits with Srikanth Vissa, Vamsee attempts to unravel the murky sociopolitical story of Stuartpuram and its notorious thief. A story like this would ideally be complex with grey areas. But this film presents black and white characters and the promising narrative is gradually diluted into an oft-repeated mainstream story. 
+
+When director Vamsee’s ambitiously mounted Telugu film Tiger Nageswara Rao (TNR) begins with a note ‘inspired from true rumours’ and a voiceover informs us that it is a tale of blood and tears, it indicates the tone of the film. Ravi Teja plays the title character inspired by the real-life dacoit of Stuartpuram in Andhra Pradesh, Nageswara Rao who earned the moniker ‘tiger’ for his daredevil acts. In the absence of substantial documentation, the director and his team relied on accounts shared by the late dacoit’s wife and a few others in the village and the police. Sharing the writing credits with Srikanth Vissa, Vamsee attempts to unravel the murky sociopolitical story of Stuartpuram and its notorious thief. A story like this would ideally be complex with grey areas. But this film presents black and white characters and the promising narrative is gradually diluted into an oft-repeated mainstream story. 
+
+
+    </p>
+
+    </div>
+  );
+}
+
+export default App;
